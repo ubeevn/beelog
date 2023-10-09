48804378a542f5b7c4a79fe187e3a1349c508252
Trung <hi@trung.me>
2023-10-09 16:34:16 +0700

    fix can not cache

--- config/logging.php
+++ config/logging.php
@@ -1,17 +1,21 @@
 <?php
 
 use Monolog\Handler\FilterHandler;
-use Monolog\Handler\TelegramBotHandler;
+//use Monolog\Handler\TelegramBotHandler;
+use UBee\UBeeLog\TelegramBotHandler;
 use UBee\UBeeLog\JsonFormatter;
 
 return [
+    'tele_key' => env('TELEGRAM_API_KEY'),
+    'tele_channel' => env('TELEGRAM_API_CHANNEL'),
     'channels' => [
         "telegram" => [
             'driver'  => 'monolog',
             'handler' => FilterHandler::class,
             'level' => 'debug',
             'with' => [
-                'handler' => new TelegramBotHandler($apiKey = env('TELEGRAM_API_KEY'), $channel = env('TELEGRAM_CHANNEL'))
+                //'handler' => new TelegramBotHandler($apiKey = env('TELEGRAM_API_KEY'), $channel = env('TELEGRAM_CHANNEL'))
+                'handler' => TelegramBotHandler::class
             ],
             'formatter' => JsonFormatter::class,
         ],
---
+++ src/TelegramBotHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UBee\UBeeLog;
+
+use Monolog\Handler\TelegramBotHandler as BaseHandler;
+
+class TelegramBotHandler extends BaseHandler
+{
+    public function __construct()
+    {
+        parent::__construct(config('ubl.tele_key'), config('ubl.tele_channel'));
+    }
+}
