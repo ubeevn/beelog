29db7e97994ba47ecb839bf151fbd77c3072ea16
Trung <hi@trung.me>
2023-10-09 22:08:05 +0700

    xx

--- config/logging.php
+++ config/logging.php
@@ -1,22 +1,21 @@
 <?php
 
 use Monolog\Handler\FilterHandler;
-//use Monolog\Handler\TelegramBotHandler;
-use UBee\UBeeLog\TelegramBotHandler;
+use Monolog\Handler\TelegramBotHandler;
+//use UBee\UBeeLog\TelegramBotHandler;
 use UBee\UBeeLog\JsonFormatter;
 
 return [
     'tele_key' => env('TELEGRAM_API_KEY'),
-    'tele_channel' => env('TELEGRAM_API_CHANNEL'),
+    'tele_channel' => env('TELEGRAM_CHANNEL'),
     'channels' => [
         "telegram" => [
             'driver'  => 'monolog',
             'handler' => FilterHandler::class,
             'level' => 'debug',
-            'with' => [
-                //'handler' => new TelegramBotHandler($apiKey = env('TELEGRAM_API_KEY'), $channel = env('TELEGRAM_CHANNEL'))
-                'handler' => TelegramBotHandler::class
-            ],
+//            'with' => [
+//                'handler' => new TelegramBotHandler($apiKey = env('TELEGRAM_API_KEY'), $channel = env('TELEGRAM_CHANNEL'))
+//            ],
             'formatter' => JsonFormatter::class,
         ],
     ]
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace UBee\UBeeLog;
 
 use Illuminate\Support\ServiceProvider as Provider;
+use Monolog\Handler\TelegramBotHandler;
 
 class ServiceProvider extends Provider
 {
@@ -10,8 +11,15 @@ class ServiceProvider extends Provider
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/logging.php','ubl');
         config(['logging.channels.telegram' => config('ubl.channels.telegram')]);
+        $with = new TelegramBotHandler(config('ubl.tele_key'), config('ubl.tele_channel'));
+        config(['logging.channels.telegram.with.handler' => $with]);
         $defaultChannels = config('logging.channels.stack.channels');
         $mergedChannels = array_merge($defaultChannels, ['telegram']);
         config(['logging.channels.stack.channels' => $mergedChannels]);
+    }
+
+    public function register()
+    {
+
     }
 }
